7dd3145731f6adf135657803b89947755530156f
Isene Abduba <isennetiyyo@gmail.com>
2025-08-09 10:01:58 +0300

    Add responsive hamburger menu with toggle animation and updated styles

---
+++ headphones/8-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+        menuToggle.classList.toggle('open');
+    });
+});
